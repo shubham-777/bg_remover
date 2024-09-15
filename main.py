e3d85ae1b2c27182ef4d55656c42fae17f81d3fa
Shubham Ahinave <codesign.developers@gmail.com>
2024-09-15 23:13:52 +0530

    feat(notify_feedback): used google smtp for main notification
    updated readme

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from app import create_app
 
 if __name__ == '__main__':
     create_app = create_app()
-    create_app.run()
+    create_app.run(host='0.0.0.0')
 else:
     gunicorn_app = create_app()
